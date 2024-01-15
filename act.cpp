0b2e594c90b98a7a03071ecccfb33fe7553f404d
Bettye <155570293+BJblue10@users.noreply.github.com>
2024-01-15 10:48:56 -0600

    Create act.cpp

---
+++ act.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // Animals on the farm 
+    int dog = 10;
+    int cat = 20;
+    int cow = 50; 
+    // Print 
+      std::cout << cat+dog - cow ; 
+    return 0;
+}
